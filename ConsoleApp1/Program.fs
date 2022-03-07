0095066b2351d63cc2d5b92d79e1fcf230875725
iamgirya <iamgirya@yandex.ru>
2022-03-07 11:46:54 +0300

    Лаба 5 Задача 2

--- ConsoleApp1/Program.fs
+++ ConsoleApp1/Program.fs
@@ -1,21 +1,35 @@
 ﻿open System
 
 
+(*  1
 let z1 ovt =
     if ovt = "F#" || ovt = "Prolog"
         then "Сударь, Вы - подлиза."
         else "Не знаю такого."
 
+[<EntryPoint>]
+let main argv =
+    printfn "Какой твой язык любимый?"
+    let s = System.Console.ReadLine()
+    printfn "%s" (z1 s)
+    0
+*)
 
 
 [<EntryPoint>]
 let main argv =
-
-(* задача 1
+    //суперпозиция
     printfn "Какой твой язык любимый?"
-    let s = System.Console.ReadLine()
-    printfn "%s" (z1 s)
-*)
+    let ifa ovt =if (ovt = "F#" || ovt = "Prolog") then "Сударь, Вы - подлиза." else "Не знаю такого."
+    ( ifa  >> printfn "%s")  (System.Console.ReadLine())   
 
+    //каррирование
+    printfn "Какой твой язык любимый?"
+    let for_true = "Сударь, Вы - подлиза."
+    let for_false = "Не знаю такого."
+    let ifa ovt t f =if (ovt = "F#" || ovt = "Prolog") then t else f
+    let redy = ifa (System.Console.ReadLine())
+    printfn "%s" (redy for_true for_false)
 
     0
+    
