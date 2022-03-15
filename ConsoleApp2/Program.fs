de2ee2463aec3f91874f1b3002225d4708d702c1
iamgirya <iamgirya@yandex.ru>
2022-03-15 21:18:10 +0300

    Задача 14

--- ConsoleApp2/Program.fs
+++ ConsoleApp2/Program.fs
@@ -164,6 +164,41 @@ let main argv =
 
 (* 14
 
+let rec inputList n =
+    if n=0 then []
+    else  
+    System.Convert.ToInt32(System.Console.ReadLine()) :: (inputList (n-1))
+
+let rec outList list = 
+    match list with
+    [] ->   
+        let z = System.Console.ReadKey()
+        0
+    | (head : int)::tail -> 
+        System.Console.WriteLine(head)
+        outList tail  
+
+let rec funcAcc list f  (p: int-> bool) (acc:int) =
+    match list with
+    [] ->
+        acc
+    | (head : int)::tail -> 
+        if (p head) then funcAcc tail f p (f acc head) 
+        else funcAcc tail f p acc
+
+let countBetweensAB list a b=
+    funcAcc list (fun x y -> x+1) (fun x -> x > a && x < b) 0
+    
+[<EntryPoint>]
+let main argv =
+//1.14
+//Дан целочисленный массив и интервал a..b. Необходимо найти
+//количество элементов в этом интервале.
+    let (a,b) = System.Convert.ToInt32(System.Console.ReadLine()) ,System.Convert.ToInt32(System.Console.ReadLine())
+    let testList = System.Convert.ToInt32(System.Console.ReadLine()) |> inputList 
+    printfn "%i" (countBetweensAB testList a b)
+    0
+
 *)
 
 (* 15
@@ -204,7 +239,7 @@ let rec outList list =
         System.Console.WriteLine(head)
         outList tail  
 
-let rec funcAcc list f  (p: int-> bool) acc =
+let rec funcAcc list f  (p: int-> bool) (acc:int) =
     match list with
     [] ->
         acc
@@ -212,68 +247,15 @@ let rec funcAcc list f  (p: int-> bool) acc =
         if (p head) then funcAcc tail f p (f acc head) 
         else funcAcc tail f p acc
 
-let foundMax list =
-    funcAcc list (fun x y -> if (y>x) then y else x) (fun x -> true) Int32.MinValue
-let foundMin list =
-    funcAcc list (fun x y -> if (y<x) then y else x) (fun x -> true) Int32.MaxValue
-
-let rec foundIndOfElem list elem index =
-    match list with
-    [] ->
-        -1
-    | (head : int)::tail -> 
-        if (head = elem) then index 
-        else foundIndOfElem tail elem (index+1)
-    
-
-let rec foundPart listik stopIndex nowIndex=
-    match listik with
-    [] ->
-        []
-    | (head : int)::tail -> 
-        if (nowIndex = stopIndex) then 
-            []
-        else head :: (foundPart tail stopIndex (nowIndex+1))
-
-let rec foundEnd listik stopIndex nowIndex=
-    match listik with
-    [] ->
-        []
-    | (head : int)::tail -> 
-        if (nowIndex = stopIndex) then 
-            head :: tail
-        else (foundEnd tail stopIndex (nowIndex+1))
-
-
-
-let rec reverseListFromTo list indexFrom indexTo nowIndex=
-    match list with
-    [] ->
-        []
-    | (head : int)::tail -> 
-        if (nowIndex = indexFrom) then 
-            let reversePart = foundPart tail indexTo (nowIndex+1) 
-            let reversePart = List.rev reversePart
-            let endPart = foundEnd tail indexTo (nowIndex+1) 
-            head :: (List.append reversePart endPart)
-             
-        else head :: (reverseListFromTo tail indexFrom indexTo (nowIndex+1))
-
-
-
-let reversePartBetweenMinAndMax list =
-    let minI = foundIndOfElem list (foundMin list) 0
-    let maxI = foundIndOfElem list (foundMax list) 0
-    if (minI > maxI) then
-        reverseListFromTo list maxI minI 0
-    else
-        reverseListFromTo list minI maxI 0
+let countBetweensAB list a b=
+    funcAcc list (fun x y -> x+1) (fun x -> x > a && x < b) 0
     
 [<EntryPoint>]
 let main argv =
-//1.12 Дан целочисленный массив. Необходимо переставить в обратном 
-//порядке элементы массива, расположенные между его минимальным и
-//максимальным элементами.
+//1.14
+//Дан целочисленный массив и интервал a..b. Необходимо найти
+//количество элементов в этом интервале.
+    let (a,b) = System.Convert.ToInt32(System.Console.ReadLine()) ,System.Convert.ToInt32(System.Console.ReadLine())
     let testList = System.Convert.ToInt32(System.Console.ReadLine()) |> inputList 
-    outList (reversePartBetweenMinAndMax testList)
+    printfn "%i" (countBetweensAB testList a b)
     0
