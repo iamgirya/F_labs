c652c9de517597058b232c7b48bf9afed61c3f41
iamgirya <iamgirya@yandex.ru>
2022-03-15 21:05:45 +0300

    Задача 13

--- ConsoleApp2/Program.fs
+++ ConsoleApp2/Program.fs
@@ -72,6 +72,94 @@ let main argv =
 
 (* 13
 
+let rec inputList n =
+    if n=0 then []
+    else  
+    System.Convert.ToInt32(System.Console.ReadLine()) :: (inputList (n-1))
+
+let rec outList list = 
+    match list with
+    [] ->   
+        let z = System.Console.ReadKey()
+        0
+    | (head : int)::tail -> 
+        System.Console.WriteLine(head)
+        outList tail  
+
+let rec funcAcc list f  (p: int-> bool) acc =
+    match list with
+    [] ->
+        acc
+    | (head : int)::tail -> 
+        if (p head) then funcAcc tail f p (f acc head) 
+        else funcAcc tail f p acc
+
+let foundMax list =
+    funcAcc list (fun x y -> if (y>x) then y else x) (fun x -> true) Int32.MinValue
+let foundMin list =
+    funcAcc list (fun x y -> if (y<x) then y else x) (fun x -> true) Int32.MaxValue
+
+let rec foundIndOfElem list elem index =
+    match list with
+    [] ->
+        -1
+    | (head : int)::tail -> 
+        if (head = elem) then index 
+        else foundIndOfElem tail elem (index+1)
+    
+
+let rec foundPart listik stopIndex nowIndex=
+    match listik with
+    [] ->
+        []
+    | (head : int)::tail -> 
+        if (nowIndex = stopIndex) then 
+            []
+        else head :: (foundPart tail stopIndex (nowIndex+1))
+
+let rec foundEnd listik stopIndex nowIndex=
+    match listik with
+    [] ->
+        []
+    | (head : int)::tail -> 
+        if (nowIndex = stopIndex) then 
+            head :: tail
+        else (foundEnd tail stopIndex (nowIndex+1))
+
+
+
+let rec reverseListFromTo list indexFrom indexTo nowIndex=
+    match list with
+    [] ->
+        []
+    | (head : int)::tail -> 
+        if (nowIndex = indexFrom) then 
+            let reversePart = foundPart tail indexTo (nowIndex+1) 
+            let reversePart = List.rev reversePart
+            let endPart = foundEnd tail indexTo (nowIndex+1) 
+            head :: (List.append reversePart endPart)
+             
+        else head :: (reverseListFromTo tail indexFrom indexTo (nowIndex+1))
+
+
+
+let reversePartBetweenMinAndMax list =
+    let minI = foundIndOfElem list (foundMin list) 0
+    let maxI = foundIndOfElem list (foundMax list) 0
+    if (minI > maxI) then
+        reverseListFromTo list maxI minI 0
+    else
+        reverseListFromTo list minI maxI 0
+    
+[<EntryPoint>]
+let main argv =
+//1.12 Дан целочисленный массив. Необходимо переставить в обратном 
+//порядке элементы массива, расположенные между его минимальным и
+//максимальным элементами.
+    let testList = System.Convert.ToInt32(System.Console.ReadLine()) |> inputList 
+    outList (reversePartBetweenMinAndMax testList)
+    0
+
 *)
 
 (* 14
@@ -107,7 +195,16 @@ let rec inputList n =
     else  
     System.Convert.ToInt32(System.Console.ReadLine()) :: (inputList (n-1))
 
-let rec funcAcc list (f : int -> int -> int) (p: int-> bool) acc =
+let rec outList list = 
+    match list with
+    [] ->   
+        let z = System.Console.ReadKey()
+        0
+    | (head : int)::tail -> 
+        System.Console.WriteLine(head)
+        outList tail  
+
+let rec funcAcc list f  (p: int-> bool) acc =
     match list with
     [] ->
         acc
@@ -117,14 +214,66 @@ let rec funcAcc list (f : int -> int -> int) (p: int-> bool) acc =
 
 let foundMax list =
     funcAcc list (fun x y -> if (y>x) then y else x) (fun x -> true) Int32.MinValue
+let foundMin list =
+    funcAcc list (fun x y -> if (y<x) then y else x) (fun x -> true) Int32.MaxValue
 
-let foundCountAfterMax list =
-    let max =  foundMax list
-    funcAcc list (fun x y -> if (y = max) then 0 elif (x <> -1) then (x+1) else -1) (fun x -> true) -1
+let rec foundIndOfElem list elem index =
+    match list with
+    [] ->
+        -1
+    | (head : int)::tail -> 
+        if (head = elem) then index 
+        else foundIndOfElem tail elem (index+1)
+    
 
+let rec foundPart listik stopIndex nowIndex=
+    match listik with
+    [] ->
+        []
+    | (head : int)::tail -> 
+        if (nowIndex = stopIndex) then 
+            []
+        else head :: (foundPart tail stopIndex (nowIndex+1))
+
+let rec foundEnd listik stopIndex nowIndex=
+    match listik with
+    [] ->
+        []
+    | (head : int)::tail -> 
+        if (nowIndex = stopIndex) then 
+            head :: tail
+        else (foundEnd tail stopIndex (nowIndex+1))
+
+
+
+let rec reverseListFromTo list indexFrom indexTo nowIndex=
+    match list with
+    [] ->
+        []
+    | (head : int)::tail -> 
+        if (nowIndex = indexFrom) then 
+            let reversePart = foundPart tail indexTo (nowIndex+1) 
+            let reversePart = List.rev reversePart
+            let endPart = foundEnd tail indexTo (nowIndex+1) 
+            head :: (List.append reversePart endPart)
+             
+        else head :: (reverseListFromTo tail indexFrom indexTo (nowIndex+1))
+
+
+
+let reversePartBetweenMinAndMax list =
+    let minI = foundIndOfElem list (foundMin list) 0
+    let maxI = foundIndOfElem list (foundMax list) 0
+    if (minI > maxI) then
+        reverseListFromTo list maxI minI 0
+    else
+        reverseListFromTo list minI maxI 0
+    
 [<EntryPoint>]
 let main argv =
-//1.1 Дан целочисленный массив. Необходимо найти количество элементов, расположенных после последнего максимального.
+//1.12 Дан целочисленный массив. Необходимо переставить в обратном 
+//порядке элементы массива, расположенные между его минимальным и
+//максимальным элементами.
     let testList = System.Convert.ToInt32(System.Console.ReadLine()) |> inputList 
-    printfn "%i" (foundCountAfterMax testList)
+    outList (reversePartBetweenMinAndMax testList)
     0
