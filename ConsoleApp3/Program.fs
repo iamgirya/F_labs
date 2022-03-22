465da1e9271806b5d542bd783c28f7b7e79f43be
iamgirya <iamgirya@yandex.ru>
2022-03-22 23:02:28 +0300

    Задача 13

--- ConsoleApp3/Program.fs
+++ ConsoleApp3/Program.fs
@@ -65,7 +65,28 @@ let main argv =
 *)
 
 (* 3
+1.22
+Дан целочисленный массив и интервал a..b. Необходимо найти
+количество минимальных элементов в этом интервале.
 
+let rec readList n = 
+    if n=0 then []
+    else
+    let Head = System.Convert.ToInt32(System.Console.ReadLine())
+    let Tail = readList (n-1)
+    Head::Tail
+
+[<EntryPoint>]
+let main argv =
+    let list1 = System.Convert.ToInt32(System.Console.ReadLine()) |> readList
+    let (a,b) = (System.Convert.ToInt32(System.Console.ReadLine()), System.Convert.ToInt32(System.Console.ReadLine()))
+    let min = List.fold (fun x y -> if (x>y) then y else x) list1.Head list1
+
+    let (firstPart,middleLastPart) = List.splitAt (a) list1
+    let (middlePart,lastPart) = List.splitAt (b-a+1) middleLastPart
+
+    printf "%i" (List.fold (fun x y -> if (y = min) then x+1 else x) 0 middlePart)
+    0
 *)
 
 (* 4
@@ -106,6 +127,10 @@ let main argv =
 
 *)
 
+1.22
+Дан целочисленный массив и интервал a..b. Необходимо найти
+количество минимальных элементов в этом интервале.
+
 let rec readList n = 
     if n=0 then []
     else
@@ -113,31 +138,14 @@ let rec readList n =
     let Tail = readList (n-1)
     Head::Tail
 
-let rec writeList list = 
-    match list with
-    [] ->
-        0
-    | (head : int)::tail -> 
-        System.Console.WriteLine(head)
-        writeList tail  
-
 [<EntryPoint>]
 let main argv =
     let list1 = System.Convert.ToInt32(System.Console.ReadLine()) |> readList
-    let minInd = 
-        let min = (List.fold (fun x y -> if (x>y) then y else x) list1.Head list1)
-        List.findIndex (fun x -> x = min) list1 
-    let maxInd = 
-        let max =List.fold (fun x y -> if (x>y) then x else y) list1.Head list1
-        List.findIndex (fun x -> x = max) list1 
-    if (minInd < maxInd) 
-    then
-        let (firstPart,middleLastPart) = List.splitAt (minInd+1) list1
-        let (middlePart,lastPart) = List.splitAt (maxInd-minInd-1) middleLastPart
-        let finalList = (List.append (List.append firstPart (List.rev middlePart)) lastPart)
-        writeList finalList
-    else
-        let (firstPart,middleLastPart) = List.splitAt (maxInd+1) list1
-        let (middlePart,lastPart) = List.splitAt (minInd-maxInd-1) middleLastPart
-        let finalList = (List.append (List.append firstPart (List.rev middlePart)) lastPart)
-        writeList finalList
+    let (a,b) = (System.Convert.ToInt32(System.Console.ReadLine()), System.Convert.ToInt32(System.Console.ReadLine()))
+    let min = List.fold (fun x y -> if (x>y) then y else x) list1.Head list1
+
+    let (firstPart,middleLastPart) = List.splitAt (a) list1
+    let (middlePart,lastPart) = List.splitAt (b-a+1) middleLastPart
+
+    printf "%i" (List.fold (fun x y -> if (y = min) then x+1 else x) 0 middlePart)
+    0
