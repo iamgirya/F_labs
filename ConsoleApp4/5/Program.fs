8060ddfc6d21c8a4aa27d25f202aa074a410f9cc
iamgirya <iamgirya@yandex.ru>
2022-04-12 00:07:12 +0300

    Задача 6

--- ConsoleApp4/5/Program.fs
+++ ConsoleApp4/5/Program.fs
@@ -18,8 +18,11 @@ type TSpasport(n:string, s:string, c: char, e: float, m: int) =
     member this.Number
         with get() = number
         and set(value) = number <- value
+    override this.ToString() = "Серия и номер: " + series + " "+ number + ", категория: "+ Convert.ToString(category) + ", мощность двигателя: "+ Convert.ToString(enginePower) + ", масса: "+ Convert.ToString(mass)
+    member this.Print() = Console.WriteLine(this.ToString())
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let p1 = TSpasport("0316", "556677", 'A', 104.15, 1375)
+    p1.Print()
     0 // return an integer exit code
