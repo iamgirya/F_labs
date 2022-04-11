e87bc60cb57f68121d0942fe3a95b476e813391c
iamgirya <iamgirya@yandex.ru>
2022-04-11 21:12:28 +0300

    Задача 2

--- ConsoleApp4/2/Program.fs
+++ ConsoleApp4/2/Program.fs
@@ -44,15 +44,26 @@ let applyFunctor p1 p2 =
 let applyFunctorTest a b c d=
     if ( take (applyFunctor a b) = (take a) (take b)
     && (applyFunctor (returnApply c) (returnApply d)) = returnApply (c d)
-    && 1 = 1 //из-за строгой типизации функций доказать третье свойство коммутативности невозможно.
-    && 1 = 1 // свойство ассоциативности тоже невозможно доказать из-за свойств языка f#
+    && true //(applyFunctor a b) = (applyFunctor b a)  из-за строгой типизации функций доказать третье свойство коммутативности для данного типа невозможно.
+    && true // свойство ассоциативности тоже невозможно доказать из-за свойств языка f#
     )
         then "Аппликативный функтор удовлетворяет законам"
         else "Аппликативный функтор не удовлетворяет законам"
+
+let monada (f:'b->Some<'b>) (a:Some<'b>) =
+    match a with
+    Just r ->
+        (f r)
+    | _ -> No
 
 [<EntryPoint>]
 let main argv =
     let s = Just(5)
     Console.WriteLine( functorTest s)
-    //Console.WriteLine( applyFunctorTest s)
+    let fs = Just(fun x -> x+3)
+    let f = fun x -> x*2
+    let n = 10
+    Console.WriteLine( applyFunctorTest fs s f n)
+
+    Console.WriteLine( monada (fun x -> Just(x+3)) s)
     0
