46dffe70b5fa325cd064548f91244b1d93995ab8
iamgirya <iamgirya@yandex.ru>
2022-04-14 21:50:15 +0300

    Задача 1 теперь полностью

--- ConsoleApp5/Flibrary/StartupForm.fs
+++ ConsoleApp5/Flibrary/StartupForm.fs
@@ -5,22 +5,3 @@ open System.Windows.Forms
 
 type StartupForm() as this = 
     inherit Form1()
-    do
-        this.button1.Click.Add <| fun _ -> 
-        try 
-            let a = double(this.richTextBox2.Text)
-            let b =  double(this.richTextBox3.Text)
-            let c =  double(this.richTextBox4.Text)   
-            let d = b*b - 4.0*a*c
-            let output = 
-                if (d > 0.0)
-                then "X1 = " + Convert.ToString((-b+Math.Sqrt(d))/(2.0*a)) + " X2 = " + Convert.ToString((-b-Math.Sqrt(d))/(2.0*a))
-                elif (d = 0.0)
-                then "X = " + Convert.ToString((-b)/(2.0*a))
-                else 
-                    let sign,supsign = if (a > 0.0) then '+','-' else '-','+'
-                    "X1 = " + Convert.ToString((-b)/(2.0*a)) + Convert.ToString(sign) + "sqrt(" + Convert.ToString((d/4.0)/(a*a)) + ") X2 = " + Convert.ToString((-b)/(2.0*a)) + Convert.ToString(supsign) + "sqrt(" + Convert.ToString((d/4.0)/(a*a)) + ")"
-            this.richTextBox1.Text <- output
-        with
-        | _ ->
-            this.richTextBox1.Text <- "Ошибка ввода!"
--- ConsoleApp5/WinFormsApp1/Form1.Designer.cs
+++ ConsoleApp5/WinFormsApp1/Form1.Designer.cs
@@ -29,80 +29,13 @@ namespace WinFormsApp1
         /// </summary>
         public void InitializeComponent()
         {
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
-            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
-            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
-            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Font = new System.Drawing.Font("Segoe UI", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.richTextBox1.Location = new System.Drawing.Point(12, 302);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(776, 136);
-            this.richTextBox1.TabIndex = 2;
-            this.richTextBox1.Text = "";
-            // 
-            // richTextBox2
-            // 
-            this.richTextBox2.Font = new System.Drawing.Font("Segoe UI", 44F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.richTextBox2.Location = new System.Drawing.Point(12, 62);
-            this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(250, 120);
-            this.richTextBox2.TabIndex = 3;
-            this.richTextBox2.Text = "1";
-            // 
-            // richTextBox3
-            // 
-            this.richTextBox3.Font = new System.Drawing.Font("Segoe UI", 44F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.richTextBox3.Location = new System.Drawing.Point(268, 62);
-            this.richTextBox3.Name = "richTextBox3";
-            this.richTextBox3.Size = new System.Drawing.Size(264, 120);
-            this.richTextBox3.TabIndex = 4;
-            this.richTextBox3.Text = "-2";
-            // 
-            // richTextBox4
-            // 
-            this.richTextBox4.Font = new System.Drawing.Font("Segoe UI", 44F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.richTextBox4.Location = new System.Drawing.Point(538, 62);
-            this.richTextBox4.Name = "richTextBox4";
-            this.richTextBox4.Size = new System.Drawing.Size(250, 120);
-            this.richTextBox4.TabIndex = 5;
-            this.richTextBox4.Text = "1";
-            // 
-            // richTextBox5
-            // 
-            this.richTextBox5.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.richTextBox5.Location = new System.Drawing.Point(12, 12);
-            this.richTextBox5.Name = "richTextBox5";
-            this.richTextBox5.Size = new System.Drawing.Size(776, 44);
-            this.richTextBox5.TabIndex = 6;
-            this.richTextBox5.Text = "aX^2                                        +bX                                  " +
-    "      +c ";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 188);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(228, 108);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "Вычислить";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.richTextBox5);
-            this.Controls.Add(this.richTextBox4);
-            this.Controls.Add(this.richTextBox3);
-            this.Controls.Add(this.richTextBox2);
-            this.Controls.Add(this.richTextBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -110,13 +43,6 @@ namespace WinFormsApp1
         }
 
         #endregion
-
-        public System.Windows.Forms.RichTextBox richTextBox1;
-        public System.Windows.Forms.RichTextBox richTextBox2;
-        public System.Windows.Forms.RichTextBox richTextBox3;
-        public System.Windows.Forms.RichTextBox richTextBox4;
-        public System.Windows.Forms.RichTextBox richTextBox5;
-        public System.Windows.Forms.Button button1;
     }
 }
 
