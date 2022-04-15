f462a18b2f674e0e28fb784172ab61f2ce387d6d
iamgirya <iamgirya@yandex.ru>
2022-04-15 09:32:59 +0300

    Задача 5

---
+++ ConsoleApp5/Flibrary5/StartupForm.fs
@@ -0,0 +1,9 @@
+﻿namespace WinFormsApp1
+open WinFormsApp1
+open System
+open System.Windows.Forms
+
+type StartupForm() as this = 
+    inherit Form1(Width= 800, Height = 400, 
+    Visible = true, 
+    Text = "Кортежи в F#")
